cafa5c8a7fe8485161202c4bcc7b37410e3a96fb
Dave <dave_roweuk@yahoo.co.uk>
2018-07-11 18:22:12 +0100

    Trying to load Mapbox

--- assets/js/lw-custom.js
+++ assets/js/lw-custom.js
@@ -15,6 +15,17 @@ var getJSON = function (url, callback) {
 	xhr.send();
 };
 
+var addCSS = function (url) {
+	var head = document.getElementsByTagName('head')[0];
+	var link = document.createElement('link');
+	link.id = cssId;
+	link.rel = 'stylesheet';
+	link.type = 'text/css';
+	link.href = url;
+	link.media = 'all';
+	head.appendChild(link);
+};
+
 var updateDates = function () {
 	// Get the current page
 	var data_attributes = document.querySelectorAll('[data-updated]');
@@ -38,11 +49,31 @@ var updateDates = function () {
 			}
 		});
 	}
-}
+};
+
+var createMemberMap = function () {
+	mapboxgl.accessToken = 'pk.eyJ1IjoiZHhyb3dlIiwiYSI6ImNqMnI5Y2p2cDAwMHQzMm11cjZlOGQ2b2oifQ.uxhJoz3QCO6cARRQ8uKdzw';
+
+	const map = new mapboxgl.Map({
+		container: "map",
+		style: 'mapbox://styles/mapbox/streets-v9'
+	});
+
+	map.addControl(new mapboxgl.FullscreenControl());
+
+};
 
 document.addEventListener("DOMContentLoaded", function () {
+
 	// Get the current page
 	var path = window.location.pathname;
 
-	if (path === '/data/') updateDates();
+	if (path === '/data/') {
+		updateDates();
+	}
+	if (path === '/map/') {
+		addCSS('https://cdnjs.cloudflare.com/ajax/libs/mapbox-gl/0.46.0/mapbox-gl.css');
+		addCSS('/assets/css/lw-custom.css');
+		createMemberMap();
+	}
 });
